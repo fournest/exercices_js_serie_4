7dc47f8c5659366817077ab8057df989d288f156
fournest sebastien <sebfour85@gmail.com>
2025-05-19 18:45:10 +0200

    jusqu'à exercice 2 à corriger

---
+++ script.js
@@ -0,0 +1,18 @@
+// exercice 1
+const nombres = [1, 2, 3, 4, 5];
+for (let index = 0; index < nombres.length; index++) {
+    console.log(nombres[index]);
+}
+
+// exercice 2
+function sommeTableau(tableau) {
+    let somme = 0;
+    for (let index = 0; index < tableau.length; index++) {
+    }
+    return somme;
+}
+
+const tableau = [1, 2, 3, 4, 5];
+const resultat = sommeTableau(tableau);
+
+console.log(`La somme est ${resultat}`);
